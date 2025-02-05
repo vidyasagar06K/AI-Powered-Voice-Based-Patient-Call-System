e628a5c294d1e6942fb9baf37a4851985eabf897
Vidya Sagar Kodi <145068198+vidyasagar06K@users.noreply.github.com>
2025-02-05 14:34:18 +0530

    Update patientRoutes.js

--- backend-careconnect/CareConnect-backend/routes/patientRoutes.js
+++ backend-careconnect/CareConnect-backend/routes/patientRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { addPatient, getPatient, deletePatient, loginPatient } from '../controller/patientController.js';
+import { addPatient, getPatient, deletePatient, loginPatient, uploadAudio, upload } from '../controller/patientController.js';
 const router = Router();
 
 router.post('/', addPatient);
 router.get('/', getPatient);
 router.delete('/:patientId', deletePatient);
 router.post('/login', loginPatient);
-
+router.post('/upload-audio', upload, uploadAudio);
 
 export default router;
